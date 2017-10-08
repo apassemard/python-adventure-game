d8c7868cf7e54a3ba28a364da0d714bc58e79f22
Antony Passemard <apassemard@apassemard-macbookpro.roam.corp.google.com>
2017-10-07 22:31:16 -0700

    adding Lesson 4 files

---
+++ Lesson4/monsters.py
@@ -0,0 +1,39 @@
+bestiary={
+    'rat' : {
+        'class1' : [  #This is a basic Giant Rat
+            { 'hitroll' : 17 },
+            { 'hit_against_player' : 12 },
+            { 'damage_dice' : 3 },
+            { 'dice_rolls' : 1 },
+            { 'hp' : 2 },
+            { 'xp' : 5 },
+        ],
+    },
+    'goblin' : {
+        'class1' : [  #This is a basic Goblin
+            { 'hitroll' : 16 },
+            { 'hit_against_player' : 12 },
+            { 'damage_dice' : 6 },
+            { 'dice_rolls' : 1 },
+            { 'hp' : 5 },
+            { 'xp' : 5 },
+        ],
+        'class2' : [  #This is a stronger Goblin
+            { 'hitroll' : 17 },
+            { 'hit_against_player' : 14 },
+            { 'damage_dice' : 8 },
+            { 'dice_rolls' : 1 },
+            { 'hp' : 8 },
+            { 'xp' : 80 },
+        ],
+    },
+    'rust_monster' : {
+        'class1' :  [ #An adult rust monster
+            { 'hitroll' : 17 },
+            { 'hit_against_player' : 6 },
+            { 'dice_rolls' : 2 },
+            { 'hp' : 15 },
+            { 'xp' : 300 },
+         ]
+    }
+}
