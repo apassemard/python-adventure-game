937618dadb661fa62c59e577457b51e3a95254e8
Antony Passemard <apassemard@apassemard-macbookpro.roam.corp.google.com>
2017-10-22 20:30:52 -0700

    Update to Lesson 4 files

--- Lesson4/monsters.py
+++ Lesson4/monsters.py
@@ -1,87 +1,72 @@
 bestiary={
-    'rat' : {
-        'card' :
-          [
-            { 'AC' : 12 },
-            { 'HP' : 7 },
-            { 'CHA' : 4 },
-            { 'CON' : 11 },
-            { 'DEX' : 13 },
-            { 'INT' : 2 },
-            { 'STR' : 7 },
-            { 'WIS' : 10 },
-            { 'damage_dice' : 1 },
-            { 'dice_rolls' : 4 },
-            { 'damage_adjustment' : 2 },
-            { 'xp' : 25 },
-         ],
-    },
-    'goblin' : {
-        'card' :
-          [
-            { 'AC' : 12 },
-            { 'HP' : 7 },
-            { 'CHA' : 11 },
-            { 'CON' : 10 },
-            { 'DEX' : 12 },
-            { 'INT' : 10 },
-            { 'STR' : 8 },
-            { 'WIS' : 14 },
-            { 'damage_dice' : 1 },
-            { 'dice_rolls' : 4 },
-            { 'damage_adjustment' : 1 },
-            { 'xp' : 25 },
-         ],
-    },
-    'goblin_captain' : {
-        'card' :
-          [
-            { 'AC' : 10 },
-            { 'HP' : 9 },
-            { 'CHA' : 12 },
-            { 'CON' : 12 },
-            { 'DEX' : 12 },
-            { 'INT' : 11 },
-            { 'STR' : 9 },
-            { 'WIS' : 14 },
-            { 'damage_dice' : 1 },
-            { 'dice_rolls' : 6 },
-            { 'damage_adjustment' : 2 },
-            { 'xp' : 50 },
-         ],
-    },
-    'rust_monster' : {
-        'card' :
-          [
-            { 'AC' : 18 },
-            { 'HP' : 6 },
-            { 'CHA' : 8 },
-            { 'CON' : 13 },
-            { 'DEX' : 17 },
-            { 'INT' : 2 },
-            { 'STR' : 10 },
-            { 'WIS' : 13 },
-            { 'damage_dice' : 1 },
-            { 'dice_rolls' : 4 },
-            { 'damage_adjustment' : 0 },
-            { 'xp' : 50 },
-         ],
-    },
-    'skeleton' : {
-        'card' :
-          [
-            { 'AC' : 13 },
-            { 'HP' : 13 },
-            { 'CHA' : 5 },
-            { 'CON' : 15 },
-            { 'DEX' : 14 },
-            { 'INT' : 6 },
-            { 'STR' : 10 },
-            { 'WIS' : 8 },
-            { 'damage_dice' : 1 },
-            { 'dice_rolls' : 6 },
-            { 'damage_adjustment' : 0 },
-            { 'xp' : 50 },
-         ],
-       }
+ 'rat' : {
+   'card' :
+     {
+        'AC' : 12 ,
+        'HP' : 7 ,
+        'CON' : 11 ,
+        'DEX' : 13 ,
+        'STR' : 7 ,
+        'damage_dice' : 1 ,
+        'dice_rolls' : 4 ,
+        'damage_adjustment' : 2 ,
+        'xp' : 25 ,
+     },
+  },
+ 'goblin' : {
+   'card' :
+       {
+         'AC' : 12 ,
+         'HP' : 7 ,
+         'CON' : 10 ,
+         'DEX' : 12 ,
+         'STR' : 8 ,
+         'damage_dice' : 1 ,
+         'dice_rolls' : 4 ,
+         'damage_adjustment' : 1 ,
+         'xp' : 25 ,
+       },
+  },
+ 'goblin_captain' : {
+   'card' :
+       {
+         'AC' : 10 ,
+         'HP' : 9 ,
+         'CON' : 12 ,
+         'DEX' : 12 ,
+         'STR' : 9 ,
+         'damage_dice' : 1 ,
+         'dice_rolls' : 6 ,
+         'damage_adjustment' : 2 ,
+         'xp' : 50 ,
+       },
+   },
+ 'rust_monster' : {
+   'card' :
+       {
+         'AC' : 18 ,
+         'HP' : 6 ,
+         'CON' : 13 ,
+         'DEX' : 17 ,
+         'STR' : 10 ,
+         'damage_dice' : 1 ,
+         'dice_rolls' : 4 ,
+         'damage_adjustment' : 0 ,
+         'xp' : 50 ,
+       },
+   },
+ 'skeleton' : {
+   'card' :
+       {
+         'AC' : 13 ,
+         'HP' : 13 ,
+         'CON' : 15 ,
+         'DEX' : 14 ,
+         'STR' : 10 ,
+         'damage_dice' : 1 ,
+         'dice_rolls' : 6 ,
+         'damage_adjustment' : 0 ,
+         'xp' : 50 ,
+       },
+  }
 }
